898b922281bbe2c184de473aeea52031d56d4bd0
javon <jnegahba@calpoly.edu>
2017-06-25 11:28:22 -0700

    Renovated custom css, settings now uses radio buttons for pomodoro minutes, short break minutes, and long break minutes

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const rainforest = new Audio('rainforest.mp3')
 const Timer = Vue.component('Timer', {
   template: `
   <div class="centered">
-      <h1>Timer</h1>
-      <h4>{{pomodoros}} Pomodoros</h4>
-      <h4>{{stateStr}}</h4>
-      <h1 class="timer">{{time}}</h1>
-      <button v-if="running" @click="stop" class="button-primary wide-button">Stop</button>
-      <button v-else @click="start" class="button-primary wide-button">Start</button>
-      <button @click="reset" class="button-primary wide-button">Reset</button>
+    <div class="header">Timer</div>
+    <div class="subheader">{{pomodoros}} Pomodoros</div>
+    <div class="subheader">{{stateStr}}</div>
+    <div class="mammoth">{{time}}</div>
+    <button v-if="running" @click="stop" class="button-primary wide-button">Stop</button>
+    <button v-else @click="start" class="button-primary wide-button">Start</button>
+    <button @click="reset" class="button-primary wide-button">Reset</button>
   </div>`,
   data: function() {
     return {
@@ -129,11 +129,13 @@ const Timer = Vue.component('Timer', {
     }
   },
   created: function() {
+    console.log('creating timer')
     this.ticking = slowTicking
     this.alarm = bells
     this.state = this.states.pomodoro
     this.seconds = this.pomodoroSeconds
     ee.on('modifyPomodoroMinutes', function(m) {
+      console.log('timer: ' + m)
       this.pomodoroMinutes = m
     }.bind(this))
     ee.on('modifyShortBreakMinutes', function(m) {
@@ -161,25 +163,26 @@ const Timer = Vue.component('Timer', {
 const Tasks = Vue.component('Tasks', {
   template: `
   <div class="centered">
-    <h1>Tasks</h1>
+    <div class="header">Tasks</div>
     <div class="progress-label">{{progress}}%</div>
     <div class="progress-bar">
       <div :style="{width:progress + '%'}"></div>
     </div>
+    {{tasks}}
     <draggable v-model="tasks">
       <div v-for="(task, index) in tasks">
+        <button @click="deleteTask(index)" class="remove-button"></button>
         <input type="checkbox" v-model="task.completed" :id="index"/>
         <label :for="index" class="check"/>
         <input type="number" v-model.number="task.points" min="1" oninput="validity.valid||(value=1)"/>
         <input type="text" v-model="task.name"/>
-        <button @click="deleteTask(index)" class="remove-button"></button>
       </div>
     </draggable>
-    <span class="task-label">Points</span>
-    <input type="number" value="1" v-model.number="addPoints" min="1" oninput="validity.valid||(value=1)"/>
-    <span class="task-label">Name</span>
-    <input type="text" v-model="addName"/>
     <button @click="addTask(addName, addPoints)" class="add-button"></button>
+    <div class="label">Points</span>
+    <input type="number" value="1" v-model.number="addPoints" min="1" oninput="validity.valid||(value=1)"/>
+    <div class="label">Name</span>
+    <input type="text" v-model="addName"/>
   </div>`,
   data: function() {
     return {
@@ -220,10 +223,6 @@ const Tasks = Vue.component('Tasks', {
     }
   },
   methods: {
-    validatePoints: function(e) {
-      console.log(e)
-      return false
-    },
     addTask: function(name, points) {
       this.tasks.push({
         name: name,
@@ -242,44 +241,95 @@ const Tasks = Vue.component('Tasks', {
 const Settings = Vue.component('Settings', {
   template: `
   <div class="centered">
-    <h1>Settings</h1>
-    <div>
-      <label>Pomodoro Minutes</label>
-      <input type="number" v-model.number="pomodoroMinutes" min="1" oninput="validity.valid||(value=1)"/>
+    <div class="header">Settings</div>
+    <div class="bottom-pad">
+      <div class="subheader">Pomodoro Minutes</div>
+      <input type="radio" id="pomodoroMinutes15" name="pomodoroMinutes" value="15" v-model.number="pomodoroMinutes">
+      <label for="pomodoroMinutes15" class="radio"/>
+      <div class="radio-label">15</div>
+      <input type="radio" id="pomodoroMinutes20" name="pomodoroMinutes" value="20" v-model.number="pomodoroMinutes">
+      <label for="pomodoroMinutes20" class="radio"/>
+      <div class="radio-label">20</div>
+      <input type="radio" id="pomodoroMinutes25" name="pomodoroMinutes" value="25" v-model.number="pomodoroMinutes">
+      <label for="pomodoroMinutes25" class="radio"/>
+      <div class="radio-label">25</div>
+      <input type="radio" id="pomodoroMinutes30" name="pomodoroMinutes" value="30" v-model.number="pomodoroMinutes">
+      <label for="pomodoroMinutes30" class="radio"/>
+      <div class="radio-label">30</div>
+      <input type="radio" id="pomodoroMinutes40" name="pomodoroMinutes" value="40" v-model.number="pomodoroMinutes">
+      <label for="pomodoroMinutes40" class="radio"/>
+      <div class="radio-label">40</div>
+      <input type="radio" id="pomodoroMinutes50" name="pomodoroMinutes" value="50" v-model.number="pomodoroMinutes">
+      <label for="pomodoroMinutes50" class="radio"/>
+      <div class="radio-label">50</div>
     </div>
-    <div>
-      <label>Short Break Minutes</label>
-      <input type="number" v-model.number="shortBreakMinutes" min="1" oninput="validity.valid||(value=1)"/>
+    <div class="bottom-pad">
+      <div class="subheader">Short Break Minutes</div>
+      <input type="radio" id="shortBreakMinutes5" name="shortBreakMinutes" value="5" v-model.number="shortBreakMinutes">
+      <label for="shortBreakMinutes5" class="radio"/>
+      <div class="radio-label">5</div>
+      <input type="radio" id="shortBreakMinutes10" name="shortBreakMinutes" value="10" v-model.number="shortBreakMinutes">
+      <label for="shortBreakMinutes10" class="radio"/>
+      <div class="radio-label">10</div>
+      <input type="radio" id="shortBreakMinutes15" name="shortBreakMinutes" value="15" v-model.number="shortBreakMinutes">
+      <label for="shortBreakMinutes15" class="radio"/>
+      <div class="radio-label">15</div>
     </div>
-    <div>
-      <label>Long Break Minutes</label>
-      <input type="number" v-model.number="longBreakMinutes" min="1" oninput="validity.valid||(value=1)"/>
+    <div class="bottom-pad">
+      <div class="subheader">Long Break Minutes</div>
+      <input type="radio" id="longBreakMinutes15" name="longBreakMinutes" value="15" v-model.number="longBreakMinutes">
+      <label for="longBreakMinutes15" class="radio"/>
+      <div class="radio-label">15</div>
+      <input type="radio" id="longBreakMinutes30" name="longBreakMinutes" value="30" v-model.number="longBreakMinutes">
+      <label for="longBreakMinutes30" class="radio"/>
+      <div class="radio-label">30</div>
+      <input type="radio" id="longBreakMinutes45" name="longBreakMinutes" value="45" v-model.number="longBreakMinutes">
+      <label for="longBreakMinutes45" class="radio"/>
+      <div class="radio-label">45</div>
+      <input type="radio" id="longBreakMinutes60" name="longBreakMinutes" value="60" v-model.number="longBreakMinutes">
+      <label for="longBreakMinutes60" class="radio"/>
+      <div class="radio-label">60</div>
     </div>
-    <div>
-      <label>Ticking</label>
-      <input type="radio" name="ticking" value="noTicking" v-model="tickingRadio">No Ticking
-      <input type="radio" name="ticking" value="slowTicking" v-model="tickingRadio">Slow Ticking
-      <input type="radio" name="ticking" value="fastTicking" v-model="tickingRadio">Fast Ticking
-      <input type="radio" name="ticking" value="heartbeat" v-model="tickingRadio">Heartbeat
-      <input type="radio" name="ticking" value="rainforest" v-model="tickingRadio">Rainforest
+    <div class="bottom-pad">
+      <div class="subheader">Ticking</div>
+      <input type="radio" id="noTicking" name="ticking" value="noTicking" v-model="tickingRadio">
+      <label for="noTicking" class="radio"/>
+      <div class="radio-label">No Ticking</div>
+      <input type="radio" id="slowTicking" name="ticking" value="slowTicking" v-model="tickingRadio">
+      <label for="slowTicking" class="radio"/>
+      <div class="radio-label">Slow Ticking</div>
+      <input type="radio" id="fastTicking" name="ticking" value="fastTicking" v-model="tickingRadio">
+      <label for="fastTicking" class="radio"/>
+      <div class="radio-label">Fast Ticking</div>
+      <input type="radio" id="heartbeat" name="ticking" value="heartbeat" v-model="tickingRadio">
+      <label for="heartbeat" class="radio"/>
+      <div class="radio-label">Heartbeat</div>
+      <input type="radio" id="rainforest" name="ticking" value="rainforest" v-model="tickingRadio">
+      <label for="rainforest" class="radio"/>
+      <div class="radio-label">Rainforest</div>
     </div>
-    <div>
-      <label>Alarm</label>
-      <input type="radio" name="alarm" value="noAlarm" v-model="alarmRadio">No Alarm
-      <input type="radio" name="alarm" value="bells" v-model="alarmRadio">Bells
+    <div class="bottom-pad">
+      <div class="subheader">Alarm</div>
+      <input type="radio" id="noAlarm" name="alarm" value="noAlarm" v-model="alarmRadio">
+      <label for="noAlarm" class="radio"/>
+      <div class="radio-label">No Alarm</div>
+      <input type="radio" id="bells" name="alarm" value="bells" v-model="alarmRadio">
+      <label for="bells" class="radio"/>
+      <div class="radio-label">Bells</div>
     </div>
   </div>`,
   data: function() {
     return {
-      pomodoroMinutes: 25,
-      shortBreakMinutes: 5,
-      longBreakMinutes: 15,
+      pomodoroMinutes: null,
+      shortBreakMinutes: null,
+      longBreakMinutes: null,
       tickingRadio: null,
       alarmRadio: null
     }
   },
   watch: {
     pomodoroMinutes: function(m) {
+      console.log('watch: ' + m)
       ee.emit('modifyPomodoroMinutes', m)
     },
     shortBreakMinutes: function(m) {
@@ -296,6 +346,9 @@ const Settings = Vue.component('Settings', {
     }
   },
   created: function() {
+    this.pomodoroMinutes = 25
+    this.shortBreakMinutes = 5
+    this.longBreakMinutes = 15
     this.tickingRadio = 'slowTicking'
     this.alarmRadio = 'bells'
   }
@@ -304,20 +357,20 @@ const Settings = Vue.component('Settings', {
 const About = Vue.component('About', {
   template: `
   <div class="centered">
-    <h1>About</h1>
-    <h4>Overview</h4>
+    <div class="header">About</div>
+    <div class="subheader">Overview</div>
     <p>This is a productivity web app which hosts a pomodoro timer, a tasks page with a progress bar, and a settings page.</p>
-    <h4>Pomodoro Technique</h4>
+    <div class="subheader">Pomodoro Technique</div>
     <p>The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. The technique uses a timer to break down work into intervals, traditionally 25 minutes in length, separated by short breaks. These intervals are named pomodoros, the plural in English of the Italian word pomodoro (tomato), after the tomato-shaped kitchen timer that Cirillo used as a university student.</p>
-    <h4>Timer</h4>
+    <div class="subheader">Timer</div>
     <p>The timer adheres to the Pomdoro Technique. Whenever the timer hits 0 seconds, it plays a sound notification and moves onto its next phase automatically. The phases are as follows - after every pomodoro there is either a short break or, if you hit your fourth consecutive pomodoro, a long break. After every break, there is a pomodoro. The timer holds the number of pomodoros completed and the current state. You may start, stop, and reset the timer.</p>
-    <h4>Tasks</h4>
+    <div class="subheader">Tasks</div>
     <p>The tasks page allows you to create tasks with specified points. You may create, remove, edit, and move tasks by dragging them. A point is a discretionary unit of measure that allows you to estimate the effort of a task relative to other tasks on your list. With this information you may mark tasks as completed and the progress bar will gauge your estimated completion for the session.</p>
-    <h4>Settings</h4>
+    <div class="subheader">Settings</div>
     <p>The settings page allows you to customize the minutes of a pomodoro, short break, or long break.</p>
-    <h4>Session</h4>
+    <div class="subheader">Session</div>
     <p>Unfortunately, the web app does not support persistent storage. If you refresh the web application, your saved tasks and settings will be lost.</p>
-    <h4>Author</h4>
+    <div class="subheader">Author</div>
     <p>This web app was created by Javon Negahban.</p>
   </div>`
 })
